982fbc9131fea394b15a272a8479a607d0761f39
Filip Hana <filip.hana4@gmail.com>
2025-04-08 20:53:54 +0200

    Divided Spring MVC test code into multiple classes and modified to use json, created as of now unmodified vaadin project

---
+++ GUITests/demo2/src/main/java/org/example/demo2/Greeting.java
@@ -0,0 +1,3 @@
+package org.example.demo2;
+
+public record Greeting(long id, String content) { }
---
+++ GUITests/demo2/src/main/java/org/example/demo2/GreetingController.java
@@ -0,0 +1,31 @@
+package org.example.demo2;
+
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+// @SpringBootApplication
+@RestController // returns a domain object instead of view, shorthand for both @Controller and @ResponseBody
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    // json
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    // String
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
+
