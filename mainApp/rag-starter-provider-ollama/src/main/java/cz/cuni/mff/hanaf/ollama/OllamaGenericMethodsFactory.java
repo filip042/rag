b30b45a0d7ff644c69308c2aabbc4f90d8e82fed
Filip Hana <filip.hana4@gmail.com>
2026-01-04 02:07:21 +0100

    Removed unnecessary autoconfiguration classes, implemented fallback llm methods

--- mainApp/rag-starter-provider-ollama/src/main/java/cz/cuni/mff/hanaf/ollama/OllamaGenericMethodsFactory.java
+++ mainApp/rag-starter-provider-ollama/src/main/java/cz/cuni/mff/hanaf/ollama/OllamaGenericMethodsFactory.java
@@ -1,4 +1,28 @@
 package cz.cuni.mff.hanaf.ollama;
 
-public class OllamaGenericMethodsFactory {
+import cz.cuni.mff.hanaf.core.llm.LlmMethods;
+import cz.cuni.mff.hanaf.core.llm.LlmMethodsFactory;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class OllamaGenericMethodsFactory implements LlmMethodsFactory {
+
+    private final OllamaChatModel ollamaChatModel;
+
+    public OllamaGenericMethodsFactory(OllamaChatModel ollamaChatModel) {
+        this.ollamaChatModel = ollamaChatModel;
+    }
+
+    @Override
+    public boolean supports(String modelName) {
+        return modelName.startsWith("qwen3:");
+    }
+
+    @Override
+    public LlmMethods create(String modelName) {
+        return new OllamaGenericMethods(ollamaChatModel, modelName);
+    }
+
 }
--- mainApp/rag-starter-provider-openai/src/main/java/cz/cuni/mff/hanaf/openai/OpenAiGenericMethodsFactory.java
+++ mainApp/rag-starter-provider-openai/src/main/java/cz/cuni/mff/hanaf/openai/OpenAiGenericMethodsFactory.java
@@ -1,4 +1,27 @@
 package cz.cuni.mff.hanaf.openai;
 
-public class OpenAiGenericMethodsFactory {
+import cz.cuni.mff.hanaf.core.llm.LlmMethods;
+import cz.cuni.mff.hanaf.core.llm.LlmMethodsFactory;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class OpenAiGenericMethodsFactory implements LlmMethodsFactory {
+
+    private final OpenAiChatModel openAiChatModel;
+
+    public OpenAiGenericMethodsFactory(OpenAiChatModel openAiChatModel) {
+        this.openAiChatModel = openAiChatModel;
+    }
+
+    @Override
+    public boolean supports(String modelName) {
+        return true;
+    }
+
+    @Override
+    public LlmMethods create(String modelName) {
+        return new OpenAiGenericMethods(openAiChatModel, modelName);
+    }
 }
