43b9c60cf6ece7ee64072d949fda8b61d5f63b5a
Filip Hana <filip.hana4@gmail.com>
2026-06-08 01:01:07 +0200

    Added docstrings to files in the security package

--- mainApp/rag-app/src/main/java/cz/cuni/mff/hanaf/mainapp/security/SecurityConfig.java
+++ mainApp/rag-app/src/main/java/cz/cuni/mff/hanaf/mainapp/security/SecurityConfig.java
@@ -5,9 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * Configures the security beans.
+ */
 @Configuration
 public class SecurityConfig {
 
+    /**
+     * Returns a {@link PasswordEncoder} bean.
+     *
+     * @return the password encoder
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
